94eb0bcf98c1259b68d718d6aeca3485b1bdf721
Tim Kainz <tikaiz@gmx.at>
2024-09-20 11:24:45 +0200

    added more Types

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1. Number
+
+x = 4;
+y = 3.14;
+
+c = 24/0; //Infinity
+
+//2. String
+
+a = 'Hello';
+b = `Hello value ${x}`; //template string
+
+//3. boolean
+
+d = true;
+e = false;
+
+//4. null
+
+f = null;
+
