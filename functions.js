f94c4fc072f6aa6ca1bc9cd5b01501b52b9928b2
Tim Kainz <tikaiz@gmx.at>
2024-09-20 12:05:07 +0200

    added functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+
+// function add(a,b){ //function statement
+//     return a+b;
+// }
+
+const add = function(a,b,c){ //function expression
+    return a+b+c;
+}
+
+// console.log(add(1,2));
+console.log(add(1,2,3,4));
