ac6dd676525c146ac03ff9eee7efacdee7ca79cc
Tim Kainz <tikaiz@gmx.at>
2024-09-20 11:57:23 +0200

    more Falsy

--- falsy.js
+++ falsy.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const a = '';
+const a = null;
 
 //Falsy values: 0, '', null, undefined, false
 //Truthy values: everything else
 
-if(a){
+if(a!==null){
     console.log('This will never run')
 }
+
+// const c = 23; //--> User Input
+// const b = 42;
+// console.log(c||b); //--> Default Value, Nicht empfohlen!
+// console.log(c && b)
+
+const z = c !== 0 ? c : b;
