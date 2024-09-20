9849cb6b1526265d22a2b0998fba229565af7ca8
Tim Kainz <tikaiz@gmx.at>
2024-09-20 11:49:28 +0200

    added falsy values

--- falsy.js
+++ falsy.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const a = false;
+const a = '';
+
+//Falsy values: 0, '', null, undefined, false
+//Truthy values: everything else
+
 if(a){
     console.log('This will never run')
 }
